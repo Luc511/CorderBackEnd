e20cde752fabc61e4d2f21eee78653c137e518e6
Lucas Balon <lucasbalon@gmail.com>
2024-01-09 09:17:04 +0100

    Add duplicate participation validation and related exception
    A new method for validating uniqueness of participant's email and address has been introduced to guard against duplicate participations. Correspondingly, a new custom exception "DuplicateParticipationException" has been implemented to provide meaningful error messages when such duplicates occur. Also, "findAll" method is added into ParticipationService and its implementation and exceptions are handled in ControllerAdvisor.

--- src/main/java/be/technobel/corder/bl/ParticipationService.java
+++ src/main/java/be/technobel/corder/bl/ParticipationService.java
@@ -3,6 +3,9 @@ package be.technobel.corder.bl;
 import be.technobel.corder.dl.models.Participation;
 import be.technobel.corder.pl.models.forms.ParticipationForm;
 
+import java.util.List;
+
 public interface ParticipationService {
     Participation create(ParticipationForm participationForm);
+    List<Participation> findAll();
 }
--- src/main/java/be/technobel/corder/bl/impl/ParticipationServiceImpl.java
+++ src/main/java/be/technobel/corder/bl/impl/ParticipationServiceImpl.java
@@ -1,10 +1,14 @@
 package be.technobel.corder.bl.impl;
 
 import be.technobel.corder.bl.ParticipationService;
+import be.technobel.corder.dl.models.Address;
 import be.technobel.corder.dl.models.Participation;
 import be.technobel.corder.dl.repositories.ParticipationRepository;
+import be.technobel.corder.pl.config.exceptions.DuplicateParticipationException;
 import be.technobel.corder.pl.models.forms.ParticipationForm;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ParticipationServiceImpl implements ParticipationService {
@@ -15,8 +19,46 @@ public class ParticipationServiceImpl implements ParticipationService {
         this.participationRepository = participationRepository;
     }
 
+    private void isUniqueParticipant(Participation participation) {
+
+        String email = formatEmail(participation);
+
+        List<String> emails = findAll().stream()
+                .map(this::formatEmail)
+                .toList();
+
+        if (emails.contains(email)) {
+            throw new DuplicateParticipationException("Ce participant a déjà joué avec cet email !");
+        }
+
+        String address = participation.getAddress().toString();
+
+        List<String> addresses = findAll().stream()
+                .map(this::formatAddress)
+                .toList();
+
+        if (addresses.contains(address)) {
+            throw new DuplicateParticipationException("Ce foyer a déjà une participation !");
+        }
+    }
+
+    private String formatAddress(Participation participation) {
+        Address Address = participation.getAddress();
+        return (Address.getStreet() + Address.getCity() + Address.getPostCode()).trim().toLowerCase();
+    }
+
+    private String formatEmail(Participation participation) {
+        return (participation.getEmail()).trim().toLowerCase();
+    }
+
     @Override
     public Participation create(ParticipationForm participationForm) {
+        isUniqueParticipant(participationForm.toEntity());
         return participationRepository.save(participationForm.toEntity());
+    }
+
+    @Override
+    public List<Participation> findAll() {
+        return participationRepository.findAll();
     }
 }
