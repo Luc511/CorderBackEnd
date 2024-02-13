6e668ebfe9672a6839d0655389e2b5719b2f109e
Lucas Balon <lucasbalon@gmail.com>
2024-02-13 15:44:50 +0100

    Add configuration for dynamic user initialization
    Introduced dynamic user initialization in the `UserInit` class using the `@Value` annotation to fetch values from the `application.yml` configuration file. This enhances the flexibility of user management, as the usernames and passwords for 'Corder' and 'Cycle en Terre' can now be set in the configuration file.

--- src/main/java/be/technobel/corder/dl/datainit/UserInit.java
+++ src/main/java/be/technobel/corder/dl/datainit/UserInit.java
@@ -5,6 +5,7 @@ import be.technobel.corder.bl.services.UserService;
 import be.technobel.corder.dl.models.enums.Role;
 import be.technobel.corder.pl.config.exceptions.DuplicateUserException;
 import be.technobel.corder.pl.models.forms.UserForm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -15,24 +16,45 @@ import java.util.HashSet;
 @Configuration
 @ConditionalOnProperty(name = "api.user-init", havingValue = "true")
 public class UserInit {
+    @Value("${corder.username}")
+    private String corderUsername;
+    @Value("${corder.password}")
+    private String corderPassword;
+    @Value("${cet.username}")
+    private String cetUsername;
+    @Value("${cet.password}")
+    private String cetPassword;
 
     @Bean
     CommandLineRunner initUser(
             UserService userService
     ) {
         return args -> {
-            HashSet<Role> roles = new HashSet<>();
-            roles.add(Role.ADMIN);
+            HashSet<Role> roleCorder = new HashSet<>();
+            roleCorder.add(Role.ADMIN);
+            HashSet<Role> roleCeT = new HashSet<>();
+            roleCeT.add(Role.LOGISTIC);
             try {
                 userService.register(
                         new UserForm(
-                                "test",
-                                "Test1234=",
-                                roles
+                                corderUsername,
+                                corderPassword,
+                                roleCorder
                         )
                 );
             }catch (DuplicateUserException e) {
-                System.out.println("UserInit pas nécessaire");
+                System.out.println("Corder UserInit pas nécessaire");
+            }
+            try {
+                userService.register(
+                        new UserForm(
+                                cetUsername,
+                                cetPassword,
+                                roleCeT
+                        )
+                );
+            }catch (DuplicateUserException e) {
+                System.out.println("Cycle en Terre UserInit pas nécessaire");
             }
         };
     }
